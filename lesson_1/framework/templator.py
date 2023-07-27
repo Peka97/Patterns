674c4535fdbf0b1e38364a291b8727a0c19e689c
Peka97 <ivan.karasyov97@gmail.com>
2023-07-27 07:11:47 +0000

    Minor fix

--- lesson_1/framework/templator.py
+++ lesson_1/framework/templator.py
@@ -1,22 +1,9 @@
 from jinja2.environment import Environment
 from jinja2 import FileSystemLoader
-from jinja2 import Template
-from os.path import join
-
-
-# def render(template, folder='templates', **kwargs):
-#     fp = join(folder, template)
-
-#     with open(fp, encoding='utf-8') as f:
-#         template = Template(f.read())
-
-#     return template.render(**kwargs)
 
 
 def render(template_name, folder='templates', **kwargs):
     env = Environment()
-    # указываем папку для поиска шаблонов
     env.loader = FileSystemLoader(folder)
-    # находим шаблон в окружении
     template = env.get_template(template_name)
     return template.render(**kwargs)
